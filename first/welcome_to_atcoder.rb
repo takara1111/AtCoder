a424e490a914e1b63200e450a122fa2401f093be
takara1356 <goblin7283@gmail.com>
2019-07-03 07:38:23 +0900

    7/3 朝

--- first/welcome_to_atcoder.rb
+++ first/welcome_to_atcoder.rb
@@ -1,9 +1,6 @@
 int1 = gets.to_i
-int2 = gets.chomp.split("")
-int2, int3 = gets.chomp.split("").
+int2, int3 = gets.chomp.split(" ").map(&:to_i)
+# int2, int3 = gets.chomp.split(" ").map(&:to_i)
 str = gets.chomp!
 
-
-p int2
-# p int1 + int2 + int3 
-p str
+print("#{int1 + int2 + int3} #{str}\n")
