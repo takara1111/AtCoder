66dd95e8be2e57cfe9192a008f387801fcc887dc
takara1356 <goblin7283@gmail.com>
2019-07-18 07:57:12 +0900

    7/18朝

--- Practice/081_B.rb
+++ Practice/081_B.rb
@@ -1,8 +1,25 @@
 def isEvenArray(*ary)
   flag = true
-  
-  
+  (0..ary.size-1).each do |i|
+    if ary[i].odd?
+      flag = false
+      break
+    end
+  end
+
+  return flag
 end
 
 n = gets.to_i
-a = gets.split.map(&:to_i)
+arr = gets.chomp.split(" ").map(&:to_i)
+
+cnt = 0
+
+while isEvenArray(*arr) == true
+  cnt += 1
+  (0..arr.size-1).each do |i|
+    arr[i] /=2
+  end
+end
+
+puts cnt
