a9636616a9f6d0fb120b20ae0eba258719555062
takara1356 <goblin7283@gmail.com>
2019-07-28 15:11:53 +0900

    7/28 朝

---
+++ Practice/087_B.rb
@@ -0,0 +1,9 @@
+# a, b, c, x = 4.times.map{gets.to_i}
+# count = 0
+(0..1).each do |i|
+  p "あ"
+  (0..1).each do |j|
+    p "い"
+    (0..1)
+  end
+end
---
+++ method_practice/nesteach.rb
@@ -0,0 +1,12 @@
+a = 0
+b = 1
+c = 0
+(0..a).each do |i|
+  p i
+  (0..b).each do |j|
+    p j
+    (0..c).each do |k|
+      p k
+    end
+  end
+end
